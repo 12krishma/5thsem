36930473ea54ac1a6cfb5c2550a794cd5f04eff7
krishma <krishmadeoju@gmail.com>
2024-06-06 08:18:50 +0545

    first commit

---
+++ stringMethods.js
@@ -0,0 +1,19 @@
+var text="I am currently involved in 5th sem of Bsc.Csit";
+// alert(text);
+console.log(text);
+
+// print the length of the string
+console.log("The length of text is:",text.length);
+
+// print the character at index 5 of the given string
+console.log ("The character at index 5 is:",text.charAt(5));
+
+//print the character code at index 5 of the given string
+console.log("the character code at index 5 is:",text.charCodeAt(5));
+
+//print and write it in the document , the text between 3 and 10 index
+console.log("The text between 3 and 10 index is:",text.slice(3,10));
+document.write("The text between 3 and 10 index is: ",text.slice(3,10));
+
+//print the substring
+console.log("The substring of the text is: ",text.substring(1,4));
